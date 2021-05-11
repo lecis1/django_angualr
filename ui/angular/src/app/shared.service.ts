91943a051aedc8a21f06c722d03ad5af539f43af
lecis <lecis19521@163.com>
2021-05-11 11:11:47 +0800

    routing-modules添加路由：employee、department; shared.service.ts添加APi接口方法

--- ui/angular/src/app/shared.service.ts
+++ ui/angular/src/app/shared.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharedService {
+  readonly ApiUrl = " http://127.0.0.1:8000/";
+  readonly PhotoUrl = " http://127.0.0.1:8000/media/";
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getDepList():Observable<any[]> {
+    return this.http.get<any[]>(this.ApiUrl + 'department/');
+  }
+  addDepartment(val:any){
+    return this.http.post<any[]>(this.ApiUrl + 'department/', val);
+  }
+  updateDepartment(val:any){
+    return this.http.put<any[]>(this.ApiUrl + 'department/', val);
+  }
+  deleteDepartment(val:any){
+    return this.http.delete<any[]>(this.ApiUrl + 'department/'+val);
+  }
+
+  getEmpList():Observable<any[]>{
+    return this.http.get<any[]>(this.ApiUrl + 'employee/');
+  }
+  addEmployee(val:any){
+    return this.http.post<any[]>(this.ApiUrl + 'employee/', val);
+  }
+  updateEmployee(val:any){
+    return this.http.put<any[]>(this.ApiUrl + 'employee/', val);
+  }
+  deleteEmployee(val:any){
+    return this.http.delete<any[]>(this.ApiUrl + 'employee/'+val);
+  }
+
+  UploadPhoto(val:any){
+    return this.http.post(this.ApiUrl + 'SaveFiles/', val)
+  }
+
+  getAllDepartmentName():Observable<any[]>{
+    return this.http.get<any[]>(this.ApiUrl + 'department/');
+  }
 }
