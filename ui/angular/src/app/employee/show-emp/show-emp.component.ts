58ebbe000961dd9cbfdfc335dc7981e044a8f116
lecis <lecis19521@163.com>
2021-05-11 18:01:21 +0800

    emp的show-emp模板初步构建

--- ui/angular/src/app/employee/show-emp/show-emp.component.ts
+++ ui/angular/src/app/employee/show-emp/show-emp.component.ts
@@ -10,11 +10,48 @@ export class ShowEmpComponent implements OnInit {
 
   constructor(private service:SharedService) { }
 
-  EmployeeList: any=[]
+  EmployeeList: any=[];
+
+  ModalTitle:string;
+
+  ActivateAddEditDepComp:boolean=false;
+
+  emp:any;
 
   ngOnInit(): void {
     this.refreshEmpList();
   }
+
+  addClick(){
+    this.emp={
+      DepartmentId:0,
+      DepartmentName:""
+    }
+    this.ModalTitle="Add Department";
+    this.ActivateAddEditDepComp=true;
+  }
+
+
+  editClick(item){
+    this.emp=item;
+    this.ModalTitle="Edit Department"
+    this.ActivateAddEditDepComp=true;
+  }
+
+  deleteClick(item){
+    if(confirm('Are you sure??')){
+      this.service.deleteDepartment(item.DepartmentId).subscribe(data=>{
+        alert(data.toString());
+        this.refreshEmpList();
+      });
+    }
+  }
+
+  closeClick(){
+    this.ActivateAddEditDepComp=false;
+    this.refreshEmpList()
+  }
+
 
   refreshEmpList(){
     this.service.getEmpList().subscribe(data=>{
